c1a09299fda74182c0f58ffdad127f3917458bee
Str0ng1ch <artemkuzmi@gmail.com>
2022-08-13 00:06:40 +0300

    Updated and optimized CheckField and CheckFieldTests

--- CheckField.py
+++ CheckField.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 SIZE = 9
 
 
@@ -6,19 +7,18 @@ class CheckField:
     def __init__(self, field):
         self.field = field
         self.transposed_field = np.array(field, dtype=object).T
-        self.check = self.check_line() * self.check_square()
 
-    def check_line(self):
+    def check(self):
         for i in range(SIZE):
-            if (len(set(item for item in self.field[i] if type(item) == int)) != SIZE or 0 in self.field[i] or
-                    len(set(item for item in self.transposed_field[i] if type(item) == np.int32)) != SIZE):
+            if 0 in self.field[i] or self.check_line(i) or self.check_square(i):
                 return False
         return True
 
-    def check_square(self):
-        for i in range(0, SIZE, 3):
-            for j in range(0, SIZE, 3):
-                if len(set(item for item in list(np.array(self.field, dtype=object)
-                    [i // 3 * 3:i // 3 * 3 + 3, j // 3 * 3:j // 3 * 3 + 3].reshape(9)) if type(item) == int)) != SIZE:
-                    return False
-        return True
+    def check_line(self, i):
+        return (len({item for item in self.field[i] if len(str(item)) == 1}) != SIZE or
+                len({item for item in self.transposed_field[i] if len(str(item)) == 1}) != SIZE)
+
+    def check_square(self, i):
+        return False if i % 3 != 0 else (list(len({item for item in list(np.array(self.field, dtype=object)
+                                        [i // 3 * 3:i // 3 * 3 + 3, j:j + 3].reshape(9)) if len(str(item)) == 1})
+                                        for j in range(0, 3)) != [SIZE, SIZE, SIZE])
--- CheckfieldTests.py
+++ CheckfieldTests.py
@@ -1,5 +1,5 @@
 import unittest
-from main import CheckField
+from CheckField import CheckField
 import json
 
 with open('CheckFieldExamples.json', 'r') as filehandle:
@@ -8,25 +8,25 @@ with open('CheckFieldExamples.json', 'r') as filehandle:
 
 class Test_check_field(unittest.TestCase):
     def test1(self):
-        self.assertTrue(CheckField(test_examples['example1']).check)
+        self.assertTrue(CheckField(test_examples['example1']).check())
 
     def test2(self):
-        self.assertFalse(CheckField(test_examples['example2']).check)
+        self.assertFalse(CheckField(test_examples['example2']).check())
 
     def test3(self):
-        self.assertTrue(CheckField(test_examples['example3']).check)
+        self.assertTrue(CheckField(test_examples['example3']).check())
 
     def test4(self):
-        self.assertFalse(CheckField(test_examples['example4']).check_square())
+        self.assertFalse(CheckField(test_examples['example4']).check_square(3))
 
     def test5(self):
-        self.assertTrue(CheckField(test_examples['example5']).check_square())
+        self.assertFalse(CheckField(test_examples['example5']).check())
 
     def test6(self):
-        self.assertFalse(CheckField(test_examples['example5']).check_line())
+        self.assertTrue(CheckField(test_examples['example5']).check_line(1))
 
     def test7(self):
-        self.assertFalse(CheckField(test_examples['example6']).check)
+        self.assertFalse(CheckField(test_examples['example6']).check())
 
 
 if __name__ == "__main__":
